f4eab44c4607c2b557ba9ba4081dadab7e087f6a
Ammar Khaled <ammarkhaled2005@gmail.com>
2026-07-07 17:00:25 +0300

    Add review endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,17 @@
 <?php
+
+use App\Http\Controllers\ReviewController;
+use Illuminate\Routing\Route;
+
+
+Route::get('/schools/{school}/reviews', [ReviewController::class, 'index']);
+Route::get('/reviews/{review}', [ReviewController::class, 'show']);
+Route::get('/users/{user}/reviews', [ReviewController::class, 'userReviews']);
+
+
+Route::middleware('auth:api')->group(function () {
+    // Reviews
+    Route::post('/schools/{school}/reviews', [ReviewController::class, 'store']);
+    Route::put('/reviews/{review}', [ReviewController::class, 'update']);
+    Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
+});
