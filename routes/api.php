667eddabe966d8c12ecc486d8a79093fa48a8a4d
lojy-khaled <logykhaled019@gmail.com>
2026-07-08 01:53:16 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SchoolController;
 use App\Http\Controllers\ReviewController;
 
+// Public routes
 Route::get('/schools/{school}/reviews', [ReviewController::class, 'index']);
 Route::get('/reviews/{review}', [ReviewController::class, 'show']);
-Route::get('/users/{user}/reviews', [ReviewController::class, 'userReviews']);
 
 Route::get('/school-types', [SchoolTypeController::class, 'index']);
 Route::get('/school-types/{schoolType}', [SchoolTypeController::class, 'show']);
@@ -18,28 +18,25 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/schools/{school}/reviews', [ReviewController::class, 'store']);
     Route::put('/reviews/{review}', [ReviewController::class, 'update']);
     Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
+    Route::get('/users/{user}/reviews', [ReviewController::class, 'userReviews']);
 
-    // School Types
-    Route::post('/school-types', [SchoolTypeController::class, 'store']);
-    Route::put('/school-types/{schoolType}', [SchoolTypeController::class, 'update']);
-    Route::delete('/school-types/{schoolType}', [SchoolTypeController::class, 'destroy']);
-
-    // Schools -- for admin only
+    // Admin only
     Route::middleware('isAdmin')->group(function () {
+        // Schools
         Route::post('/schools', [SchoolController::class, 'store']);
         Route::put('/schools/{id}', [SchoolController::class, 'update']);
         Route::delete('/schools/{id}', [SchoolController::class, 'destroy']);
+
+        // School Types
+        Route::post('/school-types', [SchoolTypeController::class, 'store']);
+        Route::put('/school-types/{schoolType}', [SchoolTypeController::class, 'update']);
+        Route::delete('/school-types/{schoolType}', [SchoolTypeController::class, 'destroy']);
     });
 });
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/forgot-password', [AuthController::class, 'forgetPassword']);
-    Route::post('/reset-password/{token}/{email}', [AuthController::class, 'resetPassword'])->name('password.reset');
-    Route::middleware('auth:api')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::get('/me', [AuthController::class, 'me']);
-    });
+    Route::post('/reset-password/{token}/{email}', [AuthController::class, 'resetPassword']);
 });
+
