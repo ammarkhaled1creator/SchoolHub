b6fd9d6e90c9e88b866c78ed92c9f55250f3b96f
lojy-khaled <logykhaled019@gmail.com>
2026-07-08 03:24:08 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -50,17 +50,54 @@ Route::prefix('auth')->group(function () {
     Route::post('/reset-password/{token}/{email}', [AuthController::class, 'resetPassword']);
 });
 
-//school type
+//school type(get ,patch , post , delete)
 
 
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
+
+// Public routes
+Route::get('/schools/{school}/reviews', [ReviewController::class, 'index']);
+Route::get('/reviews/{review}', [ReviewController::class, 'show']);
+Route::get('/schools/filter', [SchoolController::class, 'filter']);
 Route::get('/school-types', [SchoolTypeController::class, 'index']);
 Route::get('/school-types/{schoolType}', [SchoolTypeController::class, 'show']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/school-types', [SchoolTypeController::class, 'store']);
-    Route::put('/school-types/{schoolType}', [SchoolTypeController::class, 'update']);
-    Route::delete('/school-types/{schoolType}', [SchoolTypeController::class, 'destroy']);
+    // Reviews
+    Route::post('/schools/{school}/reviews', [ReviewController::class, 'store']);
+    Route::put('/reviews/{review}', [ReviewController::class, 'update']);
+    Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
+
+
+    // Schools -- for admin only
+    Route::get('/users/{user}/reviews', [ReviewController::class, 'userReviews']);
+
+    // Admin only
+    Route::middleware('admin')->group(function () {
+        // Schools
+        Route::post('/schools', [SchoolController::class, 'store']);
+        Route::put('/schools/{id}', [SchoolController::class, 'update']);
+        Route::delete('/schools/{id}', [SchoolController::class, 'destroy']);
+
+        // School Types
+        Route::post('/school-types', [SchoolTypeController::class, 'store']);
+        Route::put('/school-types/{schoolType}', [SchoolTypeController::class, 'update']);
+        Route::delete('/school-types/{schoolType}', [SchoolTypeController::class, 'destroy']);
+    });
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/forgot-password', [AuthController::class, 'forgetPassword']);
+    Route::post('/reset-password/{token}/{email}', [AuthController::class, 'resetPassword']);
+});
+
 
 
 
