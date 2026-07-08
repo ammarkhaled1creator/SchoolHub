1dcdd97546d9a137ebb3ae4b68232545de335b1a
RawanAhmed00 <ahmdrwan577@gmail.com>
2026-07-08 20:48:06 +0300

    fix: add page parameter handling in getallusers method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,8 @@ class UserController extends Controller
 {
     public function getallusers()
     {
-        
+        //default number for the page is one, but if user enters any number it will be accepted
+        $page=request('page',1);
         $allusers=Cache::remember("AllUsers_page",3600,function(){
             return User::Paginate(10);
     });
